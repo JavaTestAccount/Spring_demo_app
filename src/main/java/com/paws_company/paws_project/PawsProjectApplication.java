0f9ef67453228759df107819603db97af48f803d
Java_test_account <2z7jerfrp>
2020-04-26 19:11:06 +0300

    initial commit

---
+++ src/main/java/com/paws_company/paws_project/PawsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.paws_company.paws_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paws_company/paws_project/PawsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paws_company.paws_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
